d3f52e746973ea86c4d66061c0c9c1e9f8e78cb5
QtrinoIndia <67410487+qtrino-sn@users.noreply.github.com>
2021-04-19 22:30:08 +0530

    nw

--- assets/nav-foot-common.js
+++ assets/nav-foot-common.js
@@ -9,13 +9,13 @@ document.getElementById("nav").innerHTML =
 +  '<span> Brand Name</span>'
 +'</label>'
 +'<ul id="">'
-+  '<li><a id="home" href="/home.html">Home</a></li>'
++  '<li><a id="home" href="/">Home</a></li>'
 +  '<li><a id="product" href="/product.html">Products</a></li>'
 +  '<li><a id="blog" href="/blog.html">Blogs</a></li>'
 +  '<li><a class="social" href="https://www.facebook.com/qtrino"><img src="/assets/logo/fb.png" alt="fb"></a></li>'
 +  '<li><a class="social" href="https://www.instagram.com/_qtrino"><img src="/assets/logo/instra.png" alt="instra"></a></li>'
 +  '<li><a class="social" href="https://twitter.com/qtrino1"><img src="/assets/logo/twit.png" alt="twitter"></a></li>'
-+  '<li><a style="padding-right:0px;" class="social" href="https://www.youtube.com/channel/UCSSmITU7-0-qr6D9xqlQMVA?sub_confirmation=1"><img src="/assets/logo/qtube.png" alt="youtube"></a></li>'
++  '<li><a class="social" href="https://www.youtube.com/channel/UCSSmITU7-0-qr6D9xqlQMVA?sub_confirmation=1"><img src="/assets/logo/qtube.png" alt="youtube"></a></li>'
 +'</ul>';
 
 document.getElementById("foot").innerHTML =
