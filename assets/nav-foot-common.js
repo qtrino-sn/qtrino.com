69f8d1a8b15146b44ea7350fb7c45970de1fc9af
QtrinoIndia <67410487+qtrino-sn@users.noreply.github.com>
2021-04-20 20:46:15 +0530

    nw

--- assets/nav-foot-common.js
+++ assets/nav-foot-common.js
@@ -5,7 +5,6 @@ document.getElementById("common-head").innerHTML =
 +'<meta name="viewport" content="width=device-width, initial-scale=1.0">'
 +'<link rel="shortcut icon" href="/assets/logo/qtrino.ico" type="image/x-icon">'
 +'<style>'+'#'+ current +'{background-color: #16a5b5; color:white;}'+'</style>'
-//google analytics code:
 +'<!-- Global site tag (gtag.js) - Google Analytics -->'
 +'<script async src="https://www.googletagmanager.com/gtag/js?id=UA-172973967-1"></script>'
 +'<script>'
